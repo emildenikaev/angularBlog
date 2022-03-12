f74dbc418f87a836cf752d087214d5d013af8f43
Denikaev Emil <edenikaev@mail.ru>
2022-03-12 14:33:43 +0400

    add work with firebase

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http"
 import { User } from "src/app/shared/interfaces";
-import { Observable } from "rxjs";
+import { Observable, tap } from "rxjs";
+import { environment } from "src/environments/environment";
+import { FbAuthResponce } from "src/environments/interface";
 
 @Injectable()
 export class AuthService {
@@ -12,8 +14,11 @@ export class AuthService {
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user)
-  }
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
+    }
 
   logout() {
     
@@ -23,7 +28,7 @@ export class AuthService {
     return !!this.token
   }
 
-  private setToken() {
-
+  private setToken(responce: FbAuthResponce) {
+    console.log(responce)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyD7PTBRpPvv600tQSW_7jqxnIOT39AmtMs'
 };

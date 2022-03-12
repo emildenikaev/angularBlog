f152fcb4c07dc9ba3bab059d651e0e0a29cc2899
Denikaev Emil <edenikaev@mail.ru>
2022-03-12 21:45:39 +0400

    add save token

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -3,32 +3,45 @@ import { HttpClient } from "@angular/common/http"
 import { User } from "src/app/shared/interfaces";
 import { Observable, tap } from "rxjs";
 import { environment } from "src/environments/environment";
-import { FbAuthResponce } from "src/environments/interface";
+import { FbAuthResponce } from "../../../shared/interfaces";
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient){}
 
   get token(): string {
-    return ''
+    const expDate = new Date(localStorage.getItem('fb-token-exp'))
+    if (new Date() > expDate) {
+      this.logout()
+      return null
+    }
+    return localStorage.getItem('fb-token')
   }
 
   login(user: User): Observable<any> {
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+    user.returnSecureToken = true
+    return this.http
+    .post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
       )
     }
 
   logout() {
-    
+    this.setToken(null)
   }
 
   isAuthenticated(): boolean {
     return !!this.token
   }
 
-  private setToken(responce: FbAuthResponce) {
-    console.log(responce)
+  private setToken(responce: FbAuthResponce | null) {
+    if (responce) {
+      const expDate = new Date(new Date().getTime() + +responce.expiresIn * 1000)
+      localStorage.setItem('fb-token', responce.idToken)
+      localStorage.setItem('fb-token', expDate.toString())
+    } else {
+      localStorage.clear()
+    }
   }
 }
